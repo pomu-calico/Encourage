bd4f118bb3143463fede9f3ca9ad673570d0f72a
pomu-calico <pomufuraigon@icloud.com>
2025-08-31 13:43:21 +0900

    称賛bot ver1.1

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-IS_TEST = True#称賛機能テスト：Trueの場合テスト用チャンネルに
+IS_TEST = False#称賛機能テスト：Trueの場合テスト用チャンネルに
 if IS_TEST:
     REFERRAL_CHANNEL_ID = "C095PR2DE0P"  # テスト用
     PRAISE_CHANNEL_ID = "C0965SZNRPW"    # テスト用
